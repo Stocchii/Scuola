4b0b6abd1a7b3c59c98fed2a02f8eaac83df5735
STOCCHIERO PIETRO <10934128@itisrossi.edu.it>
2025-10-07 12:52:53 +0200

    Aggiornamento

--- App.java
+++ App.java
@@ -9,33 +9,45 @@ import java.io.IOException;
 public class App {
 
     public OkHttpClient client;
-    public App() {
+
+    public App(){
         client = new OkHttpClient();
     }
-    public void doGet(){
-        Request  request = new Request.Builder()
-                .url("https://crudcrud.com/api/2aa4e4ac184f4d7e8e4b60b94c341717/pizze/")
+
+
+    public void getAllPizze() throws IOException{
+        Request request = new Request.Builder()
+                .url("https://crudcrud.com/api/9facf74ec16944dd97464a776af08f91/pizzas")
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
             if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
+            // STAMPA L'HEADER
             Headers responseHeaders = response.headers();
             for (int i = 0; i < responseHeaders.size(); i++) {
                 System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
             }
+
+            // DESERIALIZZA IL JSON DEL BODY
             Gson gson = new Gson();
             Pizza[] pizze = gson.fromJson(response.body().string(), Pizza[].class);
-            for(Pizza pizza : pizze) {
-                System.out.println(pizza.toString());
+            for (Pizza pizza : pizze) {
+                System.out.println(pizza);
+            }
         }
+
+    }
+
+
+    public void run(){
+        try {
+            getAllPizze();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
 
-    public void run(){
-        doGet();
 
     }
 }
--- Pizza.java
+++ Pizza.java
@@ -2,8 +2,10 @@ public class Pizza {
     public String nome;
     public String ingredienti;
     public double prezzo;
+
     @Override
     public String toString(){
-        return "\nNome\t\t" + nome + "\nIngredienti:\t\t" + ingredienti + "\nPrezzo\t\t" + prezzo;
+
+        return "\nNome:\t\t\t" +nome + "\nIngredienti:\t" + ingredienti + "\nPrezzo:\t\t\t" + prezzo+ "\n";
     }
 }
