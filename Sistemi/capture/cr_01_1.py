cac71bcf6d097beb2ed9ca830004f8fface8e164
pstoc <10934128@itisrossi.vi.it>
2025-10-16 11:39:19 +0200

    Aggiunta esercizio 3

--- Sistemi/capture/cr_01_1.py
+++ Sistemi/capture/cr_01_1.py
@@ -1,6 +1,7 @@
 arr = [102, 108, 97, 103, 123, 117, 103, 104, 95, 78, 117, 109, 66, 51, 114, 53, 95, 52, 49, 114, 51, 52, 100, 121, 125]
 
-for (int i = 0; i < arr.lenght; i+)
-{
-    print(chr(arr[i]))
-}
+flag=''
+for x in arr:
+    flag += chr(x)
+    
+print(flag)
--- Sistemi/capture/cr_03_3.py
+++ Sistemi/capture/cr_03_3.py
@@ -1,5 +1,17 @@
-str = 'abc'
+from base64 import b64decode
 
-vint_bytes = ord('c') + ord('b') * 256 + ord('a')*pow(256,2)
+str = 'ZmxhZ3t3NDF0XzF0c19hbGxfYjE='
+flag_1 = b64decode(str)
 
-print(vint_bytes)
+
+num = 664813035583918006462745898431981286737635929725
+
+
+from math import log
+
+flag_2 = (num).to_bytes(20, 'big')
+
+print (flag_1 + flag_2)
+
+
+
